08afc8614dcddf5030ed1cc632abd80c839117a7
kevinPfernandes <kevin.pierre.fernandes@gmail.com>
2018-03-16 12:07:20 +0100

    nouvelle entité

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class SiteController extends Controller
 {
     /**
-     * @Route("/site", name="site")
+     * @Route("/", name="index")
      */
     public function index()
     {
@@ -16,4 +16,72 @@ class SiteController extends Controller
             'controller_name' => 'SiteController',
         ]);
     }
+    /**
+     * @Route("/training", name="training")
+     */
+    public function training()
+    {
+        return $this->render('site/cours.html.twig', [
+            'controller_name' => 'SiteController',
+        ]);
+    }
+    /**
+     * @Route("/team", name="team")
+     */
+    public function team()
+    {
+        return $this->render('site/team.html.twig', [
+            'controller_name' => 'SiteController',
+        ]);
+    }
+
+    /**
+     * @Route("/event", name="event")
+     */
+    public function event()
+    {
+        return $this->render('site/event.html.twig', [
+            'controller_name' => 'SiteController',
+        ]);
+    }
+
+    /**
+     * @Route("/subscribe", name="subscibe")
+     */
+    public function subscribe()
+    {
+        return $this->render('site/subscribe.html.twig', [
+            'controller_name' => 'SiteController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function admin()
+    {
+        return $this->render('site/admin.html.twig', [
+            'controller_name' => 'SiteController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/gestiondescours", name="gestiondescours")
+     */
+    public function gestioncours()
+    {
+        return $this->render('site/admingestiondescours.html.twig', [
+            'controller_name' => 'SiteController',
+        ]);
+    }
+    /**
+     * @Route("/admin/gestiondescoachs", name="gestiondescoach")
+     */
+    public function gestioncoach ()
+    {
+        return $this->render('site/admingestiondescoachs.html.twig', [
+            'controller_name' => 'SiteController',
+        ]);
+    }
+
 }
