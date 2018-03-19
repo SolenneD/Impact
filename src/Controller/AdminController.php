7b125db902f020d3a1945bdd74f335100be702ea
SolenneD <sdaguerre75@gmail.com>
2018-03-19 12:35:16 +0100

    easy admin logout btn

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index()
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+            'path' => '/logout'
+        ]);
+    }
+}
